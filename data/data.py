2823770d63a78d3b7652981277f0be2fd6c9d57c
melissa-24 <melissalongenberger24@gmail.com>
2021-10-13 21:03:24 -0400

    saving after class

---
+++ data/data.py
@@ -0,0 +1,5 @@
+animals = [
+    {'species': 'Lion', 'animalName': 'Simba'},
+    {'species': 'Tiger', 'animalName': 'Tigger'},
+    {'species': 'Bear', 'animalName': 'Yogi'}
+]
--- server.py
+++ server.py
@@ -1,11 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, session
+from env import KEY
+from data.data import *
 
 app = Flask(__name__)
+app.secret_key = KEY
 
 @app.route('/')
 def index():
-    pass
+    return render_template("index.html")
 
+@app.route('/addPark', methods=['POST'])
+def createPark():
+    print("Got your form submission")
+    print(request.form)
+    session['park'] = request.form['parkName']
+    return redirect('/parks')
+
+@app.route('/parks')
+def parks():
+    return render_template('parks.html', animals=animals)
+
+@app.route('/clear')
+def clear():
+    session.clear()
+    return redirect('/')
 
 
 
